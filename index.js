ace9cd20ea63750141fdd49563f762161413fd96
chenlingjie <chenlingjie@cd121.com>
2017-12-26 11:36:25 +0800

    xg

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var Concat = require('concat-with-sourcemaps');
 
 // file can be a vinyl file object or a string
 // when a string it will construct a new one
+//TODO file就是插件参数，"all.js"或{path:"all.js"}
 module.exports = function(file, opt) {
   if (!file) {
     throw new Error('gulp-concat: Missing file option');
@@ -23,7 +24,7 @@ module.exports = function(file, opt) {
   var latestMod;
   var fileName;
   var concat;
-
+  //TODO 得到配置的文件名
   if (typeof file === 'string') {
     fileName = file;
   } else if (typeof file.path === 'string') {
@@ -31,9 +32,10 @@ module.exports = function(file, opt) {
   } else {
     throw new Error('gulp-concat: Missing path in file options');
   }
-
+  //TODO 流的转换函数
   function bufferContents(file, enc, cb) {
     // ignore empty files
+    //TODO 没有文件，一般都会有这句
     if (file.isNull()) {
       cb();
       return;
@@ -63,14 +65,15 @@ module.exports = function(file, opt) {
     if (!concat) {
       concat = new Concat(isUsingSourceMaps, fileName, opt.newLine);
     }
-
+    //TODO file.contents内容
     // add file to concat instance
     concat.add(file.relative, file.contents, file.sourceMap);
     cb();
   }
-
+  //TODO 流结束之前被调用，之前的函数是拼接，输出在这里
   function endStream(cb) {
     // no files passed in, no file goes out
+    //TODO 没有经过
     if (!latestFile || !concat) {
       cb();
       return;
@@ -92,10 +95,10 @@ module.exports = function(file, opt) {
     if (concat.sourceMapping) {
       joinedFile.sourceMap = JSON.parse(concat.sourceMap);
     }
-
+    //TODO 把流添加进来给下一个插件使用
     this.push(joinedFile);
     cb();
   }
-
+  //TODO 返回，第一个是转换函数，第二个函数在流结束之前被调用
   return through.obj(bufferContents, endStream);
 };
